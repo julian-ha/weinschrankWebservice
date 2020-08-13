e4220ea89da39e623315d4c15b57c91305cc6316
Julian Häring <Julian.Haering@plan-b-gmbh.com>
2020-08-13 21:52:01 +0200

    asfd

---
+++ handlebars-helpers.js
@@ -0,0 +1,10 @@
+module.exports = {
+    checkBool: function(dataa, options){
+        if (dataa == 0) {
+            this.dataa = false
+        } else {
+            this.dataa = true
+        }
+    }
+  }  
+  
